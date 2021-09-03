824cad261bc77cea2e3158efdd6f5d12a3a22ae5
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2021-09-03 09:55:27 +0200

    StartingPoint

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import listEndpoints from 'express-list-endpoints'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import { badRequestErrorHandler, notFoundErrorHandler, catchAllErrorHandler } from './utilities/errorHandlers.js'
+
+
+
+const server = express()
+
+
+server.use(cors())
+server.use(express.json())
+
+
+server.use(badRequestErrorHandler)
+server.use(notFoundErrorHandler)
+server.use(catchAllErrorHandler)
+
+const { PORT } = process.env
+
+
+
+mongoose.connect(process.env.MONGO_CONNECTION)
+
+mongoose.connection.on("connected", () => {
+    console.log("✅ Successfully connected to MONGO!");
+    server.listen(PORT, () => {
+        console.log(`✅ Server is up and running on PORT: ${PORT}`)
+        console.table(listEndpoints(server))
+    })
+})
+
+mongoose.connection.on("error", (err) => {
+    console.log(`The connection is unsuccessful!, ${err}`);
+})
