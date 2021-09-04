d9ac91f4209a7e6d8bc1077be46e4ce3c966d2b2
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2021-09-04 08:12:46 +0100

    set up

--- src/server.js
+++ src/server.js
@@ -4,15 +4,19 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 import { badRequestErrorHandler, notFoundErrorHandler, catchAllErrorHandler } from './utilities/errorHandlers.js'
 import cartsRouter from './services/carts/index.js'
-
+import productRouter from './services/products/index.js'
+import reviewsRouter from './services/reviews/index.js'
+import usersRouter from './services/users/index.js'
 
 const server = express()
-
 
 server.use(cors())
 server.use(express.json())
 
+server.use("/products", productRouter)
+server.use('/users', usersRouter)
 server.use('/carts', cartsRouter)
+server.use('/reviews', reviewsRouter)
 
 server.use(badRequestErrorHandler)
 server.use(notFoundErrorHandler)
@@ -20,9 +24,7 @@ server.use(catchAllErrorHandler)
 
 const { PORT } = process.env
 
-
-
-mongoose.connect(process.env.MONGO_CONNECTION)
+mongoose.connect(process.env.MONGODB_URI)
 
 mongoose.connection.on("connected", () => {
     console.log("✅ Successfully connected to MONGO!");
---
+++ src/services/reviews/index.js
@@ -0,0 +1,74 @@
+import express from "express"
+import createError from "http-errors"
+import ReviewModel from "./schema.js"
+import productModel from "../products/schema.js"
+
+const reviewsRouter = express.Router()
+
+reviewsRouter.post("/", async (req, res, next) => {
+  // product-details>index => sends comment by user to product ID { userId to be passed in req.body }
+  try {
+    const newReview = new ReviewModel(req.body)
+    const { _id } = await newReview.save()
+    const product = await productModel.findByIdAndUpdate(req.body.product[0], { $push: { reviews: _id } })
+    res.status(201).send({ _id, product })
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+reviewsRouter.get("/", async (req, res, next) => {
+  try {
+    const reviews = await ReviewModel.find({}).populate("product")
+    res.send(reviews)
+  } catch (error) {
+    next(error)
+  }
+})
+
+reviewsRouter.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const review = await ReviewModel.findById(id)
+    if (review) {
+      res.send(review)
+    } else {
+      next(createError(404, `Review with id ${id} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+reviewsRouter.put("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const modifiedReview = await ReviewModel.findByIdAndUpdate(id, req.body, {
+      new: true
+    })
+    if (modifiedReview) {
+      res.send(modifiedReview)
+    } else {
+      next(createError(404, `Review with id ${id} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+reviewsRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const deletedReview = await ReviewModel.findByIdAndDelete(id)
+    if (deletedReview) {
+      res.status(204).send()
+    } else {
+      next(createError(404, `Review with id ${id} not found!`))
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default reviewsRouter
