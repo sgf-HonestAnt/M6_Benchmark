c265cf6d9d4175a334ec9e82d6d8845c81d61c3d
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2021-09-05 14:02:20 +0100

    populate product at get review by id

--- src/services/reviews/index.js
+++ src/services/reviews/index.js
@@ -12,7 +12,30 @@ reviewsRouter.post("/", async (req, res, next) => {
     const { _id } = await newReview.save() 
     // this is saving, but not pushing the _id to product model.
     // Err code: 
-    const product = await productModel.findByIdAndUpdate(req.body.product[0], { $push: { reviews: _id } })
+    // 2021-09-05T12:48:34.497311+00:00 app[web.1]: CastError: Cast to ObjectId failed for value "6" (type string) at path "_id" for model "Product"
+    // 2021-09-05T12:48:34.497319+00:00 app[web.1]:     at model.Query.exec (/app/node_modules/mongoose/lib/query.js:4540:21)
+    // 2021-09-05T12:48:34.497319+00:00 app[web.1]:     at model.Query.Query.then (/app/node_modules/mongoose/lib/query.js:4639:15)
+    // 2021-09-05T12:48:34.497320+00:00 app[web.1]:     at processTicksAndRejections (internal/process/task_queues.js:95:5) {
+    // 2021-09-05T12:48:34.497321+00:00 app[web.1]:   messageFormat: undefined,
+    // 2021-09-05T12:48:34.497321+00:00 app[web.1]:   stringValue: '"6"',
+    // 2021-09-05T12:48:34.497321+00:00 app[web.1]:   kind: 'ObjectId',
+    // 2021-09-05T12:48:34.497322+00:00 app[web.1]:   value: '6',
+    // 2021-09-05T12:48:34.497322+00:00 app[web.1]:   path: '_id',
+    // 2021-09-05T12:48:34.497323+00:00 app[web.1]:   reason: TypeError: Argument passed in must be a Buffer or string of 12 bytes or a string of 24 hex characters
+    // 2021-09-05T12:48:34.497340+00:00 app[web.1]:       at new ObjectId (/app/node_modules/bson/lib/objectid.js:61:23)
+    // 2021-09-05T12:48:34.497341+00:00 app[web.1]:       at castObjectId (/app/node_modules/mongoose/lib/cast/objectid.js:25:12)
+    // 2021-09-05T12:48:34.497343+00:00 app[web.1]:       at ObjectId.cast (/app/node_modules/mongoose/lib/schema/objectid.js:246:12)
+    // 2021-09-05T12:48:34.497343+00:00 app[web.1]:       at ObjectId.SchemaType.applySetters (/app/node_modules/mongoose/lib/schematype.js:1122:12)
+    // 2021-09-05T12:48:34.497344+00:00 app[web.1]:       at ObjectId.SchemaType._castForQuery (/app/node_modules/mongoose/lib/schematype.js:1553:15)
+    // 2021-09-05T12:48:34.497344+00:00 app[web.1]:       at ObjectId.SchemaType.castForQuery (/app/node_modules/mongoose/lib/schematype.js:1543:15)
+    // 2021-09-05T12:48:34.497344+00:00 app[web.1]:       at ObjectId.SchemaType.castForQueryWrapper (/app/node_modules/mongoose/lib/schematype.js:1520:20)
+    // 2021-09-05T12:48:34.497346+00:00 app[web.1]:       at cast (/app/node_modules/mongoose/lib/cast.js:333:32)
+    // 2021-09-05T12:48:34.497346+00:00 app[web.1]:       at model.Query.Query.cast (/app/node_modules/mongoose/lib/query.js:4963:12)
+    // 2021-09-05T12:48:34.497347+00:00 app[web.1]:       at castQuery (/app/node_modules/mongoose/lib/query.js:4764:18),
+    // 2021-09-05T12:48:34.497347+00:00 app[web.1]:   valueType: 'string'
+    // 2021-09-05T12:48:34.497348+00:00 app[web.1]: }
+    // 2021-09-05T12:48:34.497326+00:00 heroku[router]: at=info method=POST path="/reviews" host=honestant-m6-benchmark-be.herokuapp.com request_id=0a8df890-055d-49b4-a9fd-fc8b154506cb fwd="86.141.5.12" dyno=web.1 connect=0ms service=114ms status=500 bytes=271 protocol=https
+    //   const product = await productModel.findByIdAndUpdate(req.body.product[0], { $push: { reviews: _id } })
     res.status(201).send({ _id, product })
   } catch (error) {
     next(error)
@@ -31,7 +54,7 @@ reviewsRouter.get("/", async (req, res, next) => {
 reviewsRouter.get("/:id", async (req, res, next) => {
   try {
     const id = req.params.id
-    const review = await ReviewModel.findById(id)
+    const review = await ReviewModel.findById(id).populate("product")
     if (review) {
       res.send(review)
     } else {
